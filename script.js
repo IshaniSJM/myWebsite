87c1a54be2478db12dcb6387e741e421e5a00233
Ishani Sadagopan Jeyaram Malismitha <ishanisadagopan04jm@gmail.com>
2024-06-26 15:01:12 -0500

    Improved website with buttons using javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+// Toggle animal on click
+const animalDiv = document.getElementById("animal-div");
+const animalButton = document.getElementById("show-animal");
+
+animalButton.addEventListener("click", () => 
+{
+    animalDiv.classList.toggle("hidden");
+})
+
+
+// Toggle power on click
+const powerDiv = document.getElementById("power-div");
+const powerButton = document.getElementById("show-power");
+
+powerButton.addEventListener("click", () => 
+{
+    powerDiv.classList.toggle("hidden");
+})
+
+
+// Toggle skills on click
+const skillsDiv = document.getElementById("skills-div");
+const skillsButton = document.getElementById("show-skills");
+
+skillsButton.addEventListener("click", () => 
+{
+    skillsDiv.classList.toggle("hidden");
+})
